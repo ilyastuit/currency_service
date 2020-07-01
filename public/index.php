fb517f9c969d2f52d7b1e72136c02a2c7a387a87
Ilyas <ilyastui@gmail.com>
2020-07-01 21:55:36 +0500

    Edited index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'Hello';
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response(
+    sprintf('Hello %s', htmlspecialchars($name))
+);
+
+$response->send();
